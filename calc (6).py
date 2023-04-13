2e8fec62394c18d336956b8e4aa0f396d829a891
kss <5680ch@naver.com>
2023-04-13 22:06:15 +0900

    v2

--- calc (6).py
+++ calc (6).py
@@ -10,46 +10,51 @@ def multiply(x, y):
     return x*y
 
 def division(x, y):
-    return x/y
+    try:
+        result = x/y
+        return result
+    except ZeroDivisionError:
+        print("0으로는 나눌 수 없습니다")
 
 # main function
 # 예외처리 실시
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:
+    while check >= 1 or check < 0:
         print("0: exit, 1: plus, 2: minus 3:multiply 4:division")
         try:
             check = int(input())
             if check == 1:
                 print("First Number")
-                x = int(input())
+                x = float(input())
                 print("Second Number")
-                y = int(input())
+                y = float(input())
                 print("answer : ", plus(x,y))
             elif check == 2:
                 print("First Number")
-                x = int(input())
+                x = float(input())
                 print("Second Number")
-                y = int(input())
+                y = float(input())
                 print("answer : ", minus(x,y))
             elif check==3:
                 print("First Number")
-                x = int(input())
+                x = float(input())
                 print("Second Number")
-                y = int(input())
+                y = float(input())
                 print("answer : ", multiply(x,y))
             
                 
             elif check == 4:
                 print("First Number")
-                x = int(input())
+                x = float(input())
                 print("Second Number")
-                y = int(input())
+                y = float(input())
                 print("answer : ", division(x,y))
 
             elif check == 0:
                 print("Thank you")
+                break
             
             else:
                 print("잘못된 입력")
